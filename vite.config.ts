6a140d97375deed2638b3185f9fcb372714c2aff
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2026-09-12 12:15:02 +0300

    fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { cloudflare } from "@cloudflare/vite-plugin";
-export default defineConfig({ plugins: [react(), cloudflare()], base: './' })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/presentation-library/",
+});
